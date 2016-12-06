92df026144229e90d4b58aab2cf9df604d7e466a
Donald Hutchison <git@toastymofo.net>
2016-12-06 15:25:25 +0100

    Massage formula.

--- wooget.rb
+++ wooget.rb
@@ -1,14 +1,23 @@
 class Wooget < Formula
   desc "nuget package cli for unity3d"
   homepage "https://github.com/wooga/wooget"
-  url "https://github.com/wooga/wooget/archive/wooga_wooget-2.1.0.gem.tar.gz"
-  sha256 "4ae646bff1a0f1d8e39cd45fc6bb39a88a146fd44efcd5230bf52a724f9fa34e"
+  url "https://github.com/wooga/wooget/archive/wooga_wooget-2.1.0.tar.gz"
+  sha256 "b78ccacd2d35e1a072555c4da92c2b3b3fcc86292b6aa9d7dd647624db79da6a"
 
   depends_on "mono" => :run
   depends_on "paket" => :run
 
   def install
+    ENV["GEM_HOME"] = libexec
     system "gem", "build", "wooget.gemspec"
     system "gem", "install", "wooga_wooget-#{version}.gem"
+
+    bin.install libexec/"bin/wooget"
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
+  end
+
+  test do
+    system "#{bin}/wooget"
   end
 end
+
