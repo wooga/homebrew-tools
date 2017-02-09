fb6ac97cd617bbc1e3aae2dac1583d55bbeeecab
Manfred Endres <manfred.endres@wooga.net>
2017-02-09 18:41:57 +0100

    Fix

--- Formula/wooget-testing-framework.rb
+++ Formula/wooget-testing-framework.rb
@@ -2,7 +2,7 @@ class WoogetTestingFramework < Formula
   desc "Make testing of wooget packages easier"
   homepage "https://github.com/wooga/wtf"
   url "https://github.com/wooga/wtf/archive/wooga_wtf-0.2.5.tar.gz"
-  sha256 "1c90126c1a26b55c9f56b0e53d1dca80776f9aae896bcdc316daa7680f16cad2"
+  sha256 "563000b29f3bced58c46082e7b280289da56a3f2ac2a88a1e1a3e0a28bd23516"
 
   depends_on "homebrew/fuse/ifuse" => :run
   depends_on :osxfuse => :run
@@ -10,7 +10,7 @@ class WoogetTestingFramework < Formula
   def install
     ENV["GEM_HOME"] = libexec
     system "gem", "build", "wtf.gemspec"
-    system "gem", "install", "-n", ".", "wooga_wtf-#{version}.gem"
+    system "gem", "install", "wooga_wtf-#{version}.gem"
 
     bin.install libexec/"bin/wtf"
     bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
