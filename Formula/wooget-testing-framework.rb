3dbd9ef307fd5719b1d2fea4f29da024588a2c35
Manfred Endres <manfred.endres@wooga.net>
2017-02-10 09:45:52 +0100

    Setup head,dev releases for wtf tool

--- Formula/wooget-testing-framework.rb
+++ Formula/wooget-testing-framework.rb
@@ -1,8 +1,16 @@
 class WoogetTestingFramework < Formula
   desc "Make testing of wooget packages easier"
   homepage "https://github.com/wooga/wtf"
-  url "https://github.com/wooga/wtf/archive/wooga_wtf-0.2.5.tar.gz"
-  sha256 "563000b29f3bced58c46082e7b280289da56a3f2ac2a88a1e1a3e0a28bd23516"
+  head "git@github.com:wooga/wtf.git"
+
+  url "https://github.com/wooga/wtf/archive/wooga_wtf-0.2.4.gem.tar.gz"
+  sha256 "43b3cb2db6eda2efccb86f0b2c65e71881bec5b3c06aaa9fb5e1fac821b56346"
+
+  devel do
+    url "https://github.com/wooga/wtf/archive/wooga_wtf-0.2.5.tar.gz"
+    sha256 "563000b29f3bced58c46082e7b280289da56a3f2ac2a88a1e1a3e0a28bd23516"    
+    
+  end
 
   depends_on "homebrew/fuse/ifuse" => :run
   depends_on :osxfuse => :run
