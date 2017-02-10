1a27cf861d99d7784893cc4be97708093f15180d
Manfred Endres <manfred.endres@wooga.net>
2017-02-10 10:04:44 +0100

    Update 0.2.4 version

--- Formula/wooget-testing-framework.rb
+++ Formula/wooget-testing-framework.rb
@@ -1,31 +1,30 @@
 class WoogetTestingFramework < Formula
   desc "Make testing of wooget packages easier"
   homepage "https://github.com/wooga/wtf"
-  head "git@github.com:wooga/wtf.git"
-  version "0.2.4"
+  head "https://github.com/wooga/wtf.git"
   
-  url "https://github.com/wooga/wtf/archive/wooga_wtf-0.2.4.gem.tar.gz"
-  sha256 "43b3cb2db6eda2efccb86f0b2c65e71881bec5b3c06aaa9fb5e1fac821b56346"
-
   devel do
     url "https://github.com/wooga/wtf/archive/wooga_wtf-0.2.5.tar.gz"
-    sha256 "563000b29f3bced58c46082e7b280289da56a3f2ac2a88a1e1a3e0a28bd23516"    
-
+    sha256 "563000b29f3bced58c46082e7b280289da56a3f2ac2a88a1e1a3e0a28bd23516"
+    version "0.2.5"
   end
 
-  depends_on "homebrew/fuse/ifuse" => :run
-  depends_on :osxfuse => :run
+  depends_on "homebrew/fuse/ifuse" => :optional
+  depends_on :osxfuse => :optional
 
   def install
     ENV["GEM_HOME"] = libexec
-    system "gem", "build", "wtf.gemspec"
-    system "gem", "install", "wooga_wtf-#{version}.gem"
+    
+    version_code = (build.head?) ? "*" : version
 
+    system "gem", "build", "wtf.gemspec"
+    system "gem", "install", "wooga_wtf-#{version_code}.gem"
+     
     bin.install libexec/"bin/wtf"
     bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
   end
 
   test do
-    system "#{bin}/wtf", "--version"
+    system "#{bin}/wtf"
   end
 end
