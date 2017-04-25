2e8a888dd490262f82e5cf5a0832855f7965eb2f
Manfred Endres <manfred.endres@tslarusso.de>
2017-04-25 17:39:41 +0200

    Publish unity-version-manager 1.1.0

--- Formula/unity-version-manager.rb
+++ Formula/unity-version-manager.rb
@@ -4,49 +4,25 @@ class UnityVersionManager < Formula
   head "https://github.com/wooga/unity-version-manager.git"
   
   stable do
-    url "https://github.com/wooga/unity-version-manager/archive/1.0.0.tar.gz"
-    sha256 "d5dd69867e0f691fb91ad9cfacf194d919e0f5a3ffb9655cffea6b60e562a284"
+    url "https://github.com/wooga/unity-version-manager/archive/1.1.0.tar.gz"
+    sha256 "fdc40e9888c5600b877d79b32ebc3efff1914949edf8b0419ae4555c36fb6e04"
   end
 
-  resource "activesupport" do
-    url "https://rubygems.org/gems/activesupport-4.2.8.gem"
-    sha256 "111ac36c5b3861a3546eeff5b972ae5170e8861c3ea8c035cd851627da6f2b72"
+  resource "docopt" do
+    url "https://rubygems.org/gems/docopt-0.5.0.gem"
+    sha256 "d7e793dc81e68902aae1ed6120550460b12e1fa911da131b4fd482b40e9b084b"
   end
 
-  resource "tzinfo" do
-    url "https://rubygems.org/gems/tzinfo-1.2.3.gem"
-    sha256 "9469862963cf5e8f12bb7d1cf89e681ebfee417537f6835b2e471c7c260d2817"
-  end
-
-  resource "addressable" do
-    url "https://rubygems.org/gems/addressable-2.5.1.gem"
-    sha256 "b09603b313a94fa3674d8fbaae77cc7c778e9d3cde5fea3b7c1fe447941818c5"
-  end
-
-  resource "byebug" do
-    url "https://rubygems.org/gems/byebug-9.0.6.gem"
-    sha256 "60b508d685ecbdd0ce4c8508527e893b27b4461a347564c589b0bd5c8c656ecd"
-  end
-
-  resource "faraday" do
-    url "https://rubygems.org/gems/faraday-0.12.0.1.gem"
-    sha256 "a80da46d8f4d64d011f97768894e33d3aaee25a37b8742d6197f2416857db849"
-  end
-
-  resource "octokit" do
-    url "https://rubygems.org/gems/octokit-4.7.0.gem"
-    sha256 "e4058c3142ff42a864fe2a5a5001338d367ca3527768ce9fd4fe7810d7a9cc40"
-  end
-
-  resource "pry-byebug" do
-    url "https://rubygems.org/gems/pry-byebug-3.4.2.gem"
-    sha256 "58f604526b36348aec680978172262cd5ca8767fb1dae9fa39611d2c37629853"
+  resource "plist" do
+    url "https://rubygems.org/gems/plist-3.2.0.gem"
+    sha256 "0b3dd455bde18ba7b954ff2fea4eae733f3b38b71504e723255bccc6567a70a9"
   end
 
   def install
     ENV["GEM_HOME"] = libexec/"vendor"
 
     inreplace (buildpath/"lib/uvm/version.rb"), /"([\da-z\.]+)"/, '"\1-HEAD"' if build.head?
+    inreplace (buildpath/"uvm.gemspec"), '`git ls-files -z`.split("\x0")', 'Dir["{**/}{.*,*}"]'
 
     resources.each do |r|
       r.verify_download_integrity(r.fetch)
