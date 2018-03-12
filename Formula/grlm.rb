b9630a8a44a4b9a26c346801940b9ad0be1f1a42
Manfred Endres <manfred.endres@tslarusso.de>
2018-03-12 18:01:05 +0100

    Release grlm 1.2.0

--- Formula/grlm.rb
+++ Formula/grlm.rb
@@ -1,8 +1,8 @@
 class Grlm < Formula
   desc "Monitor github rate limit of a user "
   homepage "https://github.com/Larusso/github-rate-limit-monitor"
-  url "https://github.com/Larusso/github-rate-limit-monitor/archive/v1.1.0.tar.gz"
-  sha256 "51a726e9f644be9d5759099cdb48a81627df4fa57916bfeb2c45a9d928eb836a"
+  url "https://github.com/Larusso/github-rate-limit-monitor/archive/v1.2.0.tar.gz"
+  sha256 "d26337b777f14291ce4179cc3e9a96cc6fb1f21efd1f5b56fc0a1a9c4c9ea761"
   head "https://github.com/Larusso/github-rate-limit-monitor.git"
 
   depends_on "cmake" => :build
