a915935fcd0cd7fc9117fdec4f5c8f0deae04254
Manfred Endres <manfred.endres@tslarusso.de>
2018-11-30 11:09:59 +0100

    Update android-sdk to latest version

--- Casks/android-sdk.rb
+++ Casks/android-sdk.rb
@@ -1,9 +1,9 @@
 cask 'android-sdk' do
-  version '25.2.3'
-  sha256 '593544d4ca7ab162705d0032fb0c0c88e75bd0f42412d09a1e8daa3394681dc6'
+  version '4333796'
+  sha256 'ecb29358bc0f13d7c2fa0f9290135a5b608e38434aad9bf7067d0252c160853e'
 
   # dl.google.com/android/repository was verified as official when first introduced to the cask
-  url "https://dl.google.com/android/repository/tools_r#{version}-macosx.zip"
+  url "https://dl.google.com/android/repository/sdk-tools-darwin-#{version}.zip"
   name 'android-sdk'
   homepage 'https://developer.android.com/index.html'
 
@@ -25,9 +25,9 @@ cask 'android-sdk' do
   binary "#{staged_path}/platform-tools/hprof-conv"
   binary "#{staged_path}/platform-tools/sqlite3"
 
-  binary "#{staged_path}/tools/bin/sdkmanager"
-
   binary "#{staged_path}/tools/android"
+  binary "#{staged_path}/tools/bin/archquery"
+  binary "#{staged_path}/tools/bin/avdmanager"
   binary "#{staged_path}/tools/bin/jobb"
   binary "#{staged_path}/tools/bin/lint"
   binary "#{staged_path}/tools/bin/monkeyrunner"
@@ -37,7 +37,7 @@ cask 'android-sdk' do
   binary "#{staged_path}/tools/emulator"
   binary "#{staged_path}/tools/emulator-check"
   binary "#{staged_path}/tools/mksdcard"
-  binary "#{staged_path}/tools/monitor"
+binary "#{staged_path}/tools/monitor"
 
   preflight do
 
