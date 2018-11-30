075fbb9af0e4d72e8d37ca0367ec5e061982e85e
Manfred Endres <manfred.endres@tslarusso.de>
2018-11-30 09:50:50 +0100

    Update android sdk
    still use `25.2.3` but install 28.0.3 as base build tools

--- Casks/android-sdk.rb
+++ Casks/android-sdk.rb
@@ -2,14 +2,12 @@ cask 'android-sdk' do
   version '25.2.3'
   sha256 '593544d4ca7ab162705d0032fb0c0c88e75bd0f42412d09a1e8daa3394681dc6'
 
-  # google.com/android/repository/tools_r was verified as official when first introduced to the cask
+  # dl.google.com/android/repository was verified as official when first introduced to the cask
   url "https://dl.google.com/android/repository/tools_r#{version}-macosx.zip"
   name 'android-sdk'
   homepage 'https://developer.android.com/index.html'
 
-  conflicts_with cask: 'android-platform-tools', cask: 'android-sdk'
-  
-  build_tools_version = '25.0.2'
+  build_tools_version = '28.0.3'
 
   binary "#{staged_path}/build-tools/#{build_tools_version}/aapt"
   binary "#{staged_path}/build-tools/#{build_tools_version}/aapt2"
@@ -19,7 +17,7 @@ cask 'android-sdk' do
   binary "#{staged_path}/build-tools/#{build_tools_version}/dx"
   binary "#{staged_path}/build-tools/#{build_tools_version}/llvm-rs-cc"
   binary "#{staged_path}/build-tools/#{build_tools_version}/zipalign"
-  
+
   binary "#{staged_path}/platform-tools/adb"
   binary "#{staged_path}/platform-tools/dmtracedump"
   binary "#{staged_path}/platform-tools/etc1tool"
@@ -30,26 +28,29 @@ cask 'android-sdk' do
   binary "#{staged_path}/tools/bin/sdkmanager"
 
   binary "#{staged_path}/tools/android"
-  binary "#{staged_path}/tools/ddms"
-  binary "#{staged_path}/tools/draw9patch"
+  binary "#{staged_path}/tools/bin/jobb"
+  binary "#{staged_path}/tools/bin/lint"
+  binary "#{staged_path}/tools/bin/monkeyrunner"
+  binary "#{staged_path}/tools/bin/screenshot2"
+  binary "#{staged_path}/tools/bin/sdkmanager"
+  binary "#{staged_path}/tools/bin/uiautomatorviewer"
   binary "#{staged_path}/tools/emulator"
   binary "#{staged_path}/tools/emulator-check"
-  binary "#{staged_path}/tools/emulator64-arm"
-  binary "#{staged_path}/tools/emulator64-crash-service"
-  binary "#{staged_path}/tools/emulator64-mips"
-  binary "#{staged_path}/tools/emulator64-x86"
-  binary "#{staged_path}/tools/hierarchyviewer"
-  binary "#{staged_path}/tools/jobb"
-  binary "#{staged_path}/tools/lint"
   binary "#{staged_path}/tools/mksdcard"
   binary "#{staged_path}/tools/monitor"
-  binary "#{staged_path}/tools/monkeyrunner"
-  binary "#{staged_path}/tools/screenshot2"
-  binary "#{staged_path}/tools/traceview"
-  binary "#{staged_path}/tools/uiautomatorviewer"
 
   preflight do
-    system_command "#{staged_path}/tools/bin/sdkmanager", args: ['platform-tools', "build-tools;#{build_tools_version}"], input: 'y'
+
+   system_command "#{staged_path}/tools/bin/sdkmanager", args: ['platform-tools',
+    'build-tools;26.0.3',
+    'build-tools;27.0.3',
+    'build-tools;28.0.3',
+    'platforms;android-25',
+    'platforms;android-26',
+    'platforms;android-27',
+    'platforms;android-28',
+    'extras;google;m2repository',
+    'extras;android;m2repository'], input: 'y'
   end
 
   postflight do
@@ -60,13 +61,12 @@ cask 'android-sdk' do
     FileUtils.rm("#{HOMEBREW_PREFIX}/share/android-sdk")
   end
 
-  caveats <<-EOS
-    We will install platform-tools, and build-tools for you.
-    You can control android sdk packages via the sdkmanager command.
-    You may want to add to your profile:
-      'export ANDROID_HOME=#{HOMEBREW_PREFIX}/share/android-sdk'
-
-    This operation may take up to 10 minutes depending on your internet connection.
-    Please, be patient.
-  EOS
+  caveats do
+    depends_on_java '8'
+    <<~EOS
+      You can control android sdk packages via the sdkmanager command.
+      You may want to add to your profile:
+        'export ANDROID_SDK_ROOT="#{HOMEBREW_PREFIX}/share/android-sdk"'
+    EOS
+  end
 end
