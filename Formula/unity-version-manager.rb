4edd019156bcea418bf23736b7c11c292bd02c58
Manfred Endres <manfred.endres@tslarusso.de>
2019-11-18 20:15:46 +0100

    Release unity-version-manager 2.4.0

--- Formula/unity-version-manager.rb
+++ Formula/unity-version-manager.rb
@@ -2,11 +2,11 @@ class UnityVersionManager < Formula
   desc "Tool that just manipulates a link to the current unity version"
   homepage "https://github.com/Larusso/unity-version-manager"
   head "https://github.com/Larusso/unity-version-manager.git"
-  version "2.3.0"
+  version "2.4.0"
 
   stable do
     url "https://github.com/Larusso/unity-version-manager/archive/v#{version}.tar.gz"
-    sha256 "4c34ce180a6bd9a8e678c3b886beda2ff9dc02e00539ee890283037d7e520a15"
+    sha256 "6df35730ad0649ca2699b6a4f93ee601df8eac89e86bad3028d20b8720ae6dbf"
   end
 
   depends_on "cmake" => :build
