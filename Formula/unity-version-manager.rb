e1d66c1fcec077627a5928e9a3ce6ade75f83328
Manfred Endres <manfred.endres@tslarusso.de>
2021-12-04 01:02:28 +0100

    Release unity-version-manager 2.6.0

--- Formula/unity-version-manager.rb
+++ Formula/unity-version-manager.rb
@@ -1,12 +1,12 @@
 class UnityVersionManager < Formula
-  desc "Tool that just manipulates a link to the current unity version"
+  desc "A commandline client to installa and manage Unity Editor installations"
   homepage "https://github.com/Larusso/unity-version-manager"
   head "https://github.com/Larusso/unity-version-manager.git"
-  version "2.4.1"
+  version "2.6.0"
 
   stable do
     url "https://github.com/Larusso/unity-version-manager/archive/v#{version}.tar.gz"
-    sha256 "86e3e06375ceb977673c6348358b971d3208e7e88513d9ab5ca6ad7cdfb0d0a6"
+    sha256 "5c88a14c804bfb8213cea930e6c440e6844023584699ffd4432ddd2c3c73ef52"
   end
 
   depends_on "cmake" => :build
