3573b8c5c04e862eb8cbfca0efd4bd743e737bd2
Manfred Endres <manfred.endres@wooga.net>
2022-02-15 10:51:33 +0100

    Add BigSur Bottle for cocoapods-art

--- Formula/cocoapods-art.rb
+++ Formula/cocoapods-art.rb
@@ -11,6 +11,11 @@ class CocoapodsArt < Formula
   	sha256 "b28f89c9be6269963524aa92ecfbc375ebd388f86475fd7519da196ee86b24fa"
   end
 
+  bottle do
+    rebuild 1
+    sha256 big_sur: "4be971005bbd070101ffcacac7d8b1c5cca642629b5a043140bf74029def0ab1"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "ruby" if Hardware::CPU.arm?
   
